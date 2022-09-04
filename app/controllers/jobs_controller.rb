155eed57696f2b226975151e4b444c45e41d50cf
MarioJr92 <mariorodjr92@gmail.com>
2022-09-04 09:56:57 -0400

    app quiz complete

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,25 @@
+class JobsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @jobs = Job.all
+    render 'jobs/index'
+  end
+
+  def show
+    @job = Job.find_by(id: params[:id])
+    render 'jobs/show'
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      render 'jobs/create'
+    end
+  end
+
+  private
+    def job_params
+      params.require(:job).permit(:url, :employer_name, :employer_description, :job_title, :job_description, :year_of_experience, :education_requirement, :industry, :base_salary, :employment_type_id)
+    end
+end
